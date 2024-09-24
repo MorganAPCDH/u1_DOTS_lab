1d37dfaf53016c12d852a82556118cc4be5eaf21
MorganAPCDH <morgancantu00@outlook.com>
2024-09-24 07:31:32 -0600

    I had trouble with the if statement in number one and had trouble with my functions in 2 and 3.

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,27 @@
  * DOTS: Level One
  *
  */
+
+let score = 0
+let scoreDisplay = document.querySelector(`.js-score`)
+let levelWinner = document.querySelector(`.level-winner`)
+let ball = document.querySelector(`.js-ball`)
+
+ball.addEventListener('click', () => {
+    score = score + 10
+    console.log(`Ten Points`)
+    scoreDisplay.innerText = score
+    console.log(`Score Again`)
+    if (score >= 100) {
+        //Not sure how to get it to go to 100, blanking completely 
+        //on what to write
+    };
+    levelWinner.style.opacity = 1;
+})
+
+//console.log(score)
+//console.log(scoreDisplay)
+
+
+
+    
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,22 @@
  * DOTS: Level Two
  *
  */
+
+
+let balls = document.querySelectorAll(`.js-ball`)
+let scoreDisplay = document.querySelector(`.js-score`)
+let levelWinner = document.querySelector(`.level-winner`)
+let score = 0
+balls.addEventListener(`click`, () => {
+    console.log(score + 10);
+    scoreDisplay.innerText = score
+});
+//I keep getting that my event listener isnt a function
+//and im not to sure why, if I remove All from, let balls = 
+//document.querySelectorAll(`.js-ball`) then it will partially function
+//but only with one ball
+
+console.log(levelWinner)
+console.log(scoreDisplay)
+console.log(ball)
+console.log(score)
